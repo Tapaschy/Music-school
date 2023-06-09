839ed7e3db34a2907ea29b528ec3a85eb86476d5
Tapas chowdhury <tapaschy12@gmail.com>
2023-06-09 19:05:49 +0600

    feedback done

--- src/pages/users/admin/ManageClasses.jsx
+++ src/pages/users/admin/ManageClasses.jsx
@@ -1,43 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useFilterclass from '../../../hooks/useFilterclass';
 import Swal from 'sweetalert2';
 
 const ManageClasses = () => {
-    const[classes,refetch]=useFilterclass();
-    console.log(classes);
+    const [classes, refetch] = useFilterclass();
+    const [disable, setDisable] = useState('');
+    const [inputValue, setInputValue] = useState('');
+    console.log(disable);
 
-    const handleclassStatus = (singleclass,status) =>{
+    const handleclassStatus = (singleclass, status,feedback) => {
+        setDisable(status);
         const body = {
-            status: status
-          };
-          console.log(body);
+            status: status,
+            feedback:feedback
+        };
+        console.log(body);
         fetch(`http://localhost:5000/classes/status/${singleclass._id}`, {
             method: 'PATCH',
             headers: {
                 "Content-Type": "application/json"
-              },
-              body: JSON.stringify(body)
+            },
+            body: JSON.stringify(body)
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            if(data.modifiedCount){
-                
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'success',
-                    title: `Class is ${status} Now!`,
-                    showConfirmButton: false,
-                    timer: 1500
-                  });
-                  refetch();
-                  
-            }
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.modifiedCount) {
+
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: `Class is ${status} Now!`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    refetch();
+
+                }
+            })
     }
     return (
         <div className='w-full'>
-            <h1>Total Classes:{classes.length}</h1>
+            <h1 className='text-center font-bold text-3xl'>Total Classes:{classes.length}</h1>
             <div className="overflow-x-auto">
                 <table className="table w-full">
                     {/* head */}
@@ -57,7 +61,7 @@ const ManageClasses = () => {
                         {/* row 1 */}
                         {classes.map((singleclass, index) => <tr key={singleclass._id}>
                             {console.log(singleclass)}
-                            <th>{index+1}
+                            <th>{index + 1}
                             </th>
                             <td>
                                 <div className="flex items-center space-x-3">
@@ -67,7 +71,7 @@ const ManageClasses = () => {
                                         </div>
                                     </div>
                                     <div>
-                                        <div className="font-bold">{singleclass?.name}</div>
+                                        <div className="font-bold">{singleclass?.classname}</div>
                                     </div>
                                 </div>
                             </td>
@@ -83,11 +87,30 @@ const ManageClasses = () => {
                             <td>
                                 {singleclass?.price}
                             </td>
-                            <td>{singleclass?.status}</td>
+                            <td>{singleclass.status}</td>
                             <th>
-                                <button disabled={singleclass.status === 'approve' } className="btn btn-ghost btn-xs" onClick={()=>handleclassStatus(singleclass,"approve")}>Approve</button>
-                                <button disabled={singleclass.status === 'deny' }  className="btn btn-ghost btn-xs" onClick={()=>handleclassStatus(singleclass,"deny")}>Deny </button>
-                                <button className="btn btn-ghost btn-xs">send feedback</button>
+                                <button disabled={singleclass.status === disable || singleclass.status === 'approve'} className="btn btn-ghost btn-xs" onClick={() => handleclassStatus(singleclass, "approve",singleclass.feedback)}>Approve</button>
+                                <button disabled={singleclass.status === disable || singleclass.status === 'deny'} className="btn btn-ghost btn-xs" onClick={() => handleclassStatus(singleclass, "deny",singleclass.feedback)}>Deny </button>
+                                <label htmlFor={singleclass._id} className="btn btn-ghost btn-xs">send feedback</label>
+                                {/*  */}
+
+                                
+
+                                {/* Put this part before </body> tag */}
+                                <input type="checkbox" id={singleclass._id} className="modal-toggle" />
+                                <div className="modal">
+                                    <div className="modal-box">
+                                        <h3 className="font-bold text-lg">Type your feedback</h3>
+                                        <textarea onChange={(e) => setInputValue(e.target.value)} className="textarea textarea-bordered" placeholder="Bio"></textarea>
+                                        <button className='btn' onClick={() => handleclassStatus(singleclass,singleclass.status,inputValue)}>Submit</button>
+                                        <div className="modal-action">
+                                        
+                                            <label htmlFor={singleclass._id} className="btn">Close!</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/*  */}
+
                             </th>
                         </tr>)}
 
@@ -95,6 +118,7 @@ const ManageClasses = () => {
 
 
                 </table>
+
             </div>
         </div>
     );
