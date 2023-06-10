f689a48ac67cd581b56959e83d8d0826d65e1182
Tapas chowdhury <tapaschy12@gmail.com>
2023-06-10 09:19:27 +0600

    create jwt

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,15 +1,18 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
 
 const useCart = () => {
-    const { user } = useContext(AuthContext);
+    const { user,loading } = useContext(AuthContext);
+    const [axiosSecure] = useAxiosSecure();
 
     const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
+        enabled: !loading,
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
-            return res.json();
+            const res = await axiosSecure(`/carts?email=${user?.email}`)
+            return res.data;
         },
     })
 
